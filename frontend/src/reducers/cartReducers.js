faf6bbc341916006dd957bd29f54bb797a657a12
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-01-11 20:22:45 -0800

    using localstorage to save items to cart

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,13 +1,13 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
-  switch (action.type) {
+  switch(action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
 
       const existItem = state.cartItems.find((x) => x.product === item.product);
 
-      if (existItem) {
+      if(existItem) {
         return {
           ...state,
           cartItems: state.cartItems.map((x) =>
