1644b9f069ea66a170707d14f1b33108824057cf
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-01-12 20:20:49 -0800

    creating user login routes, middleware, and user authentication/authorization

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,9 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import { getProducts, getProductById } from '../controllers/productController.js'
 const router = express.Router();
 
-// GET Request 
-// Fetch All Products 
-// Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+router.route('/').get(getProducts)
 
-    res.json(products)
-  })
-);
-
-// GET Request 
-// Fetch Single Product 
-// Public
-router.get("/:id", asyncHandler( async (req, res) => {
-  const product = await Product.findById(req.params.id)
-
-  if(product) {
-    res.json(product);
-  } else {
-      res.status(404)
-      throw new Error('Product not Found')
-  }
-
-  res.json(product);
-}));
+router.route('/:id').get(getProductById)
 
 export default router;
