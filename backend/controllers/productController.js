efd7806fa2a04b77050c39b98bb7a1d5d20ab793
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-01-28 18:42:14 -0800

    adding routes to create and update products for admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -41,4 +41,48 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts, deleteProduct };
+// POST Request
+// Create a Product
+// Private/ADMIN
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({});
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// PUT Request
+// Update a Product
+// Private/ADMIN
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
+});
+
+export { getProductById, getProducts, deleteProduct, createProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,18 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
+  .post(protect, admin, createProduct);
 
 export default router;
