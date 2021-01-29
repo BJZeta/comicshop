52e86285b4431484d31b392d39876b63b638f74f
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-01-28 18:26:00 -0800

    creating delete product button for admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,32 +1,44 @@
 import asyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
 
-// GET Request 
-// Fetch All Products 
+// GET Request
+// Fetch All Products
 // Public
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+  const products = await Product.find({});
 
-    res.json(products)
-})
+  res.json(products);
+});
 
-// GET Request 
-// Fetch Single Product 
+// GET Request
+// Fetch Single Product
 // Public
 const getProductById = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
-    if(product) {
-        res.json(product);
-      } else {
-          res.status(404)
-          throw new Error('Product not Found')
-      }
-    
-      res.json(product);
-})
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
 
-export {
-    getProductById,
-    getProducts
-}
+  res.json(product);
+});
+
+// DELETE Request
+// Delete a product
+// Private/ADMIN
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product Removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
+});
+
+export { getProductById, getProducts, deleteProduct };
