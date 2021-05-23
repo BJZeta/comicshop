1b228d8a187d4748dc1d2a193eb578135d2d1a6b
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-05-22 18:21:26 -0700

    setting up heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -12,17 +13,33 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
-app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
+}
 
 app.use(notFound);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
